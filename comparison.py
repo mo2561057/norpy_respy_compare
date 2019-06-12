07f831d568f75c81fb365457e64eb1cf8e11dbef
mo2561057 <moritz.mendel12@gmail.com>
2019-06-12 14:55:43 +0200

    both simulations run with some constraints

--- comparison.py
+++ comparison.py
@@ -6,10 +6,13 @@ import numpy as np
 
 from norpy import simulate
 from norpy.model_spec import get_random_model_specification, get_model_obj
+from respy_smm.auxiliary_depreciation import respy_ini_old_to_new
 from python.ov_simulation import ov_simulation
 from respy_smm.auxiliary_depreciation import respy_spec_old_to_new
-from respy.pre_processing.model_processing import write_init_file, read_init_file
+from respy.pre_processing.model_processing import write_init_file, read_init_file, convert_init_dict_to_attr_dict
 from respy import clsRespy
+from respy_smm.SimulationBasedEstimation import SimulationBasedEstimationCls
+from comparison_auxiliary import respy_obj_from_new_init
 
 init_path = "/home/moritz/OpenSourceEconomics/dev_norpy/ressources/model.respy.ini"
 
@@ -18,7 +21,11 @@ respy_example_dict = read_init_file(init_path)
 
 #Norpy example dict. We start witrh constrained values
 constr = {"num_types":3,
-          "intial_lagged_schooling_prob":1}
+          "num_edu_start":1,
+          "edu_range_start":np.array([9]),
+          "intial_lagged_schooling_prob":float(1),
+          "type_spec_shifts":np.zeros(9).reshape(3,3),
+          "shoskcs_cov":np.identity(3)}
 norpy_example_dict = get_random_model_specification(constr=constr)
 
 
@@ -36,7 +43,7 @@ def norpy_to_respy_spec(norpy_init,respy_init):
     out = respy_init
     #Basic stuff
     out["BASICS"]["periods"] = norpy_init["num_periods"]
-    out["BASICS"]["coeffs"] = norpy_init["delta"]
+    out["BASICS"]["coeffs"] = np.array([norpy_init["delta"]])
 
     #Common coeffs
     out["COMMON"]["coeffs"] = norpy_init["coeffs_common"]
@@ -50,8 +57,6 @@ def norpy_to_respy_spec(norpy_init,respy_init):
     #Education
     out["EDUCATION"]["coeff"] = norpy_init["coeffs_edu"]
     out["EDUCATION"]["start"] = norpy_init["edu_range_start"]
-    out["EDUCATION"]["share"] = np.array([1/len(out["EDUCATION"]["start"])]*len(out["EDUCATION"]["start"]))
-    out["EDUCATION"]["lagged"] = norpy_init["intial_lagged_schooling_prob"] #do we even need probabilities here ??
     out["EDUCATION"]["max"] = norpy_init["edu_max"]
 
     #Home
@@ -60,21 +65,22 @@ def norpy_to_respy_spec(norpy_init,respy_init):
     out["HOME"]["coeffs"][2] = norpy_init["coeffs_home"][1]
 
     #Shocks
-    cholesky = np.linalg.cholesky(norpy_init["shocks_cov"])#Wie teile ich die auf die covs auf ?
-    out["SHOCKS"]["coeffs"][0:3] = cholesky[0][0:3]
-    out["SHOCKS"]["coeffs"][3:5] = cholesky[1][2:4]
-    out["SHOCKS"]["coeffs"][5] = cholesky[2][2]
-
+    #For now nehmen wir einfach die identity
+    out["SHOCKS"]["coeffs"][0] = 1
+    out["SHOCKS"]["coeffs"][2:4] = np.ones(2)
+    out["SHOCKS"]["coeffs"][5] = 0
+    out["SHOCKS"]["coeffs"][7] = 0
+    out["SHOCKS"]["coeffs"][9] = 0
 
     #Type SHARES
     out["TYPE SHARES"]["coeffs"][0] = norpy_init["type_prob_cond_schooling"][0,1]
     out["TYPE SHARES"]["coeffs"][1] = norpy_init["type_prob_cond_schooling"][0,2]
 
     #Type Shifts
-    out["TYPE SHIFTS"]["coeffs"][0] = norpy_init["type_spec_shifts"][0][0]
-    out["TYPE SHIFTS"]["coeffs"][2:4] = norpy_init["type_spec_shifts"][0][1:3]
-    out["TYPE SHIFTS"]["coeffs"][4] = norpy_init["type_spec_shifts"][1][0]
-    out["TYPE SHIFTS"]["coeffs"][5:7] = norpy_init["type_spec_shifts"][1][1:3]
+    out["TYPE SHIFTS"]["coeffs"][0] = norpy_init["type_spec_shifts"][1][0]
+    out["TYPE SHIFTS"]["coeffs"][2:4] = norpy_init["type_spec_shifts"][1][1:3]
+    out["TYPE SHIFTS"]["coeffs"][4] = norpy_init["type_spec_shifts"][2][0]
+    out["TYPE SHIFTS"]["coeffs"][5:7] = norpy_init["type_spec_shifts"][2][1:3]
 
     #Sol details
     out["SOLUTION"]["seed"] = norpy_init["seed_emax"]
@@ -89,9 +95,15 @@ def norpy_to_respy_spec(norpy_init,respy_init):
 
 #Change the dict
 a=norpy_to_respy_spec(norpy_example_dict,respy_example_dict)
+
+
 #write new init file
-respy_obj = clsRespy(a)
+respy_obj = respy_obj_from_new_init(a)
+
+
+
+
 #Now run simulations
-sim_respy = ov_simulation(a)
+sim_respy = ov_simulation(respy_obj)
 
 sim_norpy = simulate(get_model_obj(norpy_example_dict))
